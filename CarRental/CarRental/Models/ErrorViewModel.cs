ce7416dac81d3a2786070b56f56d96692bafa001
illia-com <illia.tasminskyi@gmail.com>
2022-11-18 18:11:12 +0200

    create project

---
+++ CarRental/CarRental/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarRental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
